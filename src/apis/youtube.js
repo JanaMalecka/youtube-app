cde3d7aaac83edfb1082964cb9197bc1ddf63925
Jana Malecka <jm.jmalecka@gmail.com>
2022-05-28 10:32:29 +0200

    ready for deploy

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyA5KbA9QFO9LG8cwbrllW5WovyatA85YSM';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY,
+  },
+});
